cbda5aade01a0c8da827d105c943dbc8e553f2fe
T. Brandon Ashley <tbash@knights.ucf.edu>
2016-04-19 21:01:33 -0400

    remote rails

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,1 +1,1 @@
-export const API_ROOT = process.env.NODE_ENV === 'production' ? '<INSERT_PROD_ROOT>' : 'http://33.33.33.32:3000';
+export const API_ROOT = process.env.NODE_ENV === 'production' ? '<INSERT_PROD_ROOT>' : 'https://swagger-production.herokuapp.com/dogs';
